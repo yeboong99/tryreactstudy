e13ea20ef91f9ebf7fb83f289de7b126e25ec054
yeboong99 <yechani9905@gmail.com>
2025-11-22 02:51:50 +0900

    자식 컴포넌트에서 비구조화할당하여 데이터를 받는 방법 예시 두가지

--- src/components/PrintValue.tsx
+++ src/components/PrintValue.tsx
@@ -1,32 +1,77 @@
 {
-  /* 매개변수로 속성값을 받을 때 
+  /* 
 
-  boolean의 경우 boolean 대신 true|false 를 입력해도 된다
-  객체의 경우 구성원 별로 타입을 알려주면 된다
-  함수의 경우 속성이름에 대해(:) 매개변수와 출력의 타입을 알려주면 된다
-  이 예제에서는 함수의 내용을 string으로 바꿔 출력했는데, 더 자세한 활용법은 뒤에서 공부
+    자식 컴포넌트로 값을 전달할 때 이전 예제처럼 props라는 이름으로 객체를 받는 걸 명시적으로 해도 되지만
+    비구조화 방식으로 받을 수도 있다. 
+
+    예시 1. props를 직접 표기하지 않고 아래와 같이 받는 속성이름들을 나열한 다음 중괄호를 한 번 닫은 후에 
+    속성들의 타입을 다시 중괄호를 열어서 지정하여 바로 함수 내부에서는 바로 사용할 수 있는 형태를 만들거나 
+
+    예시 2. 매개변수 쪽의 코드가 너무 길어지는걸 원하지 않는다면 함수 내부에서 비구조화 할당을 할 수 있도록 props를 함수 내부에서 객체변수를 재정의하는 방법도 있다.
+
+    매개변수 쪽에서 props를 이용하지 않는 비구조화 할당을 하든, 
+    함수 내부에서 props를 이용하지 않는 비구조화 할당을 하든, 
+    처음 배운 방식처럼 비구조화 할당을 하지 않고 전부 명시적으로 작성하든 상황에 맞게 선택해서 알잘딱 하자.
+    
   */
 }
 
+// 예시 1.
+// export default function PrintValue({
+//   numberValue,
+//   stringValue,
+//   booleanValue,
+//   arrayValue,
+//   objectValue,
+//   handleClick,
+// }: {
+//   numberValue: number;
+//   stringValue: string;
+//   booleanValue: boolean;
+//   arrayValue: number[];
+//   objectValue: { name: string; age: number };
+//   handleClick: () => void;
+// }) {
+//   return (
+//     <>
+//       <p>number: {numberValue} </p>
+//       <p>string: {stringValue} </p>
+
+//       {/* 렌더링할 때에는 이전 출력하기 공부할 때와 마찬가지로 toString(), String(), JSON.stringify()등을 활용한다. */}
+//       <p>boolean: {booleanValue.toString()} </p>
+//       <p>array: {arrayValue} </p>
+//       <p>object: {JSON.stringify(objectValue)} </p>
+//       <p>function: {handleClick.toString()} </p>
+//     </>
+//   );
+// }
+
+// 예시 2.
 export default function PrintValue(props: {
   numberValue: number;
   stringValue: string;
-  booleanValue: boolean;
+  booleanValue: true | false;
   arrayValue: number[];
   objectValue: { name: string; age: number };
   handleClick: () => void;
 }) {
-  console.log(props);
+  const {
+    numberValue,
+    stringValue,
+    booleanValue,
+    arrayValue,
+    objectValue,
+    handleClick,
+  } = props;
+
   return (
     <>
-      <p>number: {props.numberValue} </p>
-      <p>string: {props.stringValue} </p>
-
-      {/* 렌더링할 때에는 이전 출력하기 공부할 때와 마찬가지로 toString(), String(), JSON.stringify()등을 활용한다. */}
-      <p>boolean: {props.booleanValue.toString()} </p>
-      <p>array: {props.arrayValue} </p>
-      <p>object: {JSON.stringify(props.objectValue)} </p>
-      <p>function: {props.handleClick.toString()} </p>
+      <p>number: {numberValue} </p>
+      <p>string: {stringValue} </p>
+      <p>boolean: {booleanValue.toString()} </p>
+      <p>array: {arrayValue} </p>
+      <p>object: {JSON.stringify(objectValue)} </p>
+      <p>function: {handleClick.toString()} </p>
     </>
   );
 }
