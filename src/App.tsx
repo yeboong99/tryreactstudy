fb6b8d2667912ba82ce41fcd71f86fe5670bd101
yeboong99 <yechani9905@gmail.com>
2025-11-29 01:39:22 +0900

    핸들러를 부모 컴포넌트에서 전달할 수도 있음, 단 이 예제에서는 출력되는 메시지가 같고 자식컴포넌트에서 출력할 메시지를 지정하는 형태

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+{
+  /**
+  
+  이벤트핸들러 메서드 자체를 props로 전달해버리는 방법도 있다.
+
+
+  
+  
+  */
+}
+
 import Button from "./components/ui/Button";
 
 export default function App() {
   return (
     <>
-      <Button message="영화를 재생합니다.">Movie Play</Button>
+      <Button handleClick={(message: string) => alert(message)}>
+        Play Movie
+      </Button>
+      <Button handleClick={(message: string) => alert(message)}>Upload!</Button>
     </>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,32 +1,14 @@
 import type React from "react";
 
-{
-  /** 
-   
-  props, children을 동시에 이용해보자.
-  
-  이벤트 핸들러에서 props를 읽어 사용하고,
-  내부에서 사용되는 버튼 태그의 컨텐츠를 전달받은 children을 사용한다. (사실 전달받은 컨텐츠 내용을 그대로 사용하도록 하는 거라 변화가 안보임)
-  
-  버튼은 사실 처음 렌더링될 때 컨텐츠를 가진 상태로 렌더링해야 하는 것이 일반적이므로, 컨텐츠를 작성하되, 둘 다 자식 컴포넌트에서 동시에 활용할 수 있다는 걸 알았다는 거에 만족하자.
-  
-  
-  
-*/
-}
-
-export default function Button({
-  message,
-  children,
-}: {
-  message: string;
+export default function Button(props: {
+  handleClick: (message: string) => void;
   children: React.ReactNode;
 }) {
-  const handleClick = () => alert(message);
-
   return (
     <>
-      <button onClick={handleClick}> {children} </button>
+      <button onClick={() => props.handleClick("클릭했다")}>
+        {props.children}
+      </button>
     </>
   );
 }
